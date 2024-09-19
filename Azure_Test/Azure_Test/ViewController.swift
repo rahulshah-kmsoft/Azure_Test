9a9424eee8ca9294fd598dc1c7e04c89269e4033
Bhautik <kmsoft2013@gmail.com>
2024-09-19 09:30:47 +0530

    - Inital Commit

---
+++ Azure_Test/Azure_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Azure_Test
+//
+//  Created by KMSOFT on 19/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
